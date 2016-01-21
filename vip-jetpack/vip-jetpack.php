c3ebff47757ef4342dfa19011502f15d692dbcaf
Travis CI <travis@travis-ci.com>
2016-01-21 19:02:07 +0000

    Built from Automattic/vipv2-mu-plugins@6d391a30912eabbd35a489a76df9a1d7abfe243c

--- vip-jetpack/vip-jetpack.php
+++ vip-jetpack/vip-jetpack.php
@@ -9,7 +9,20 @@
  * License: GPL2+
  */
 
+/**
+ * Enable VIP modules required as part of the platform
+ */
 require_once( __DIR__ . '/jetpack-mandatory.php' );
+
+/**
+ * Remove certain modules from the list of those that can be activated
+ * Blocks access to certain functionality that isn't compatible with the platform.
+ */
+add_filter( 'jetpack_get_available_modules', function( $modules ) {
+	unset( $modules['sitemaps'] ); // Duplicates msm-sitemaps and doesn't scale for our client's needs (https://github.com/Automattic/jetpack/issues/3314)
+
+	return $modules;
+}, 999 );
 
 /**
  * On VIP Go, we always want to use the Go Photon service, instead of WordPress.com's
@@ -17,9 +30,3 @@ require_once( __DIR__ . '/jetpack-mandatory.php' );
 add_filter( 'jetpack_photon_domain', function( $domain, $image_url ) {
 	return home_url();
 }, 2, 9999 );
-
-/**
- * Front-end SSL is support on VIP Go and in our file service,
- * and Jetpack's Photon module should respect that.
- */
-add_filter( 'jetpack_photon_reject_https', '__return_false' );
