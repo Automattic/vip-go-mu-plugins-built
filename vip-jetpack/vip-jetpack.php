fd9e7cc32152413842d6508d3f0db8d8714c38e6
Travis CI <travis@travis-ci.com>
2016-01-27 23:40:38 +0000

    Built from Automattic/vipv2-mu-plugins@ba0bcafe4702f14d4c0500287c253c913ac1581e

--- vip-jetpack/vip-jetpack.php
+++ vip-jetpack/vip-jetpack.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://github.com/Automattic/vipv2-mu-plugins/blob/master/jetpack-mandatory.php
  * Description: VIP-specific customisations to Jetpack.
  * Author: Automattic
- * Version: 1.0.1
+ * Version: 1.0.2
  * License: GPL2+
  */
 
@@ -28,6 +28,10 @@ add_filter( 'jetpack_get_available_modules', function( $modules ) {
  * On VIP Go, we always want to use the Go Photon service, instead of WordPress.com's
  */
 add_filter( 'jetpack_photon_domain', function( $domain, $image_url ) {
+	if ( ! apply_filters( 'wpcom_vip_enable_go_photon', true ) ) {
+		return $domain;
+	}
+
 	// only apply to photon images
 	if ( preg_match( '|^https?://i[0-9]+.wp.com$|', $domain ) ) {
 		return home_url();
