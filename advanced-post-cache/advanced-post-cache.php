24d2e5e71eba863df340f6c21fef59d83a6d5a6b
rebeccahum <no-reply@automattic.com>
2022-09-02 14:43:47 +0000

    Built from vip-go-mu-plugins@0c379c41df16768927a6543c16e01c4baf9fa9c0

--- advanced-post-cache/advanced-post-cache.php
+++ advanced-post-cache/advanced-post-cache.php
@@ -255,6 +255,9 @@ function dont_clear_advanced_post_cache() {
 
 add_action( 'clean_term_cache', 'clear_advanced_post_cache' );
 add_action( 'clean_post_cache', 'clear_advanced_post_cache' );
+add_action( 'added_post_meta', 'clear_advanced_post_cache' );
+add_action( 'updated_post_meta', 'clear_advanced_post_cache' );
+add_action( 'deleted_post_meta', 'clear_advanced_post_cache' );
 
 // Don't clear Advanced Post Cache for a new comment - temp core hack
 // http://core.trac.wordpress.org/ticket/15565
