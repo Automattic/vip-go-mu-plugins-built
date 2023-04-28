f2ab91f9fae47af2fefd8ee175d53ae467965678
rebeccahum <no-reply@automattic.com>
2023-04-28 14:59:52 +0000

    Built from vip-go-mu-plugins@0ffcb1aa8e3258295ff3225270517267e163c3e5

---
+++ vip-integrations/vip-block-data-api-0.2.1/vip-block-data-api.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Plugin Name: VIP Block Data API
+ * Plugin URI: https://wpvip.com
+ * Description: Access Gutenberg block data in JSON via the REST API.
+ * Author: WordPress VIP
+ * Text Domain: vip-block-data-api
+ * Version: 0.2.1
+ * Requires at least: 5.6.0
+ * Tested up to: 6.1.0
+ * Requires PHP: 7.4
+ * License: GPL-3
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * @package vip-block-data-api
+ */
+
+namespace WPCOMVIP\BlockDataApi;
+
+if ( ! defined( 'VIP_BLOCK_DATA_API_LOADED' ) ) {
+	define( 'VIP_BLOCK_DATA_API_LOADED', true );
+
+	define( 'WPCOMVIP__BLOCK_DATA_API__PLUGIN_VERSION', '0.2.1' );
+	define( 'WPCOMVIP__BLOCK_DATA_API__REST_ROUTE', 'vip-block-data-api/v1' );
+
+	// Composer dependencies
+	require_once __DIR__ . '/vendor/autoload.php';
+
+	// /wp-json/ API
+	require_once __DIR__ . '/src/rest/rest-api.php';
+
+	// Block parsing
+	require_once __DIR__ . '/src/parser/content-parser.php';
+	require_once __DIR__ . '/src/parser/block-additions/core-image.php';
+
+	// Analytics
+	require_once __DIR__ . '/src/analytics/analytics.php';
+}
