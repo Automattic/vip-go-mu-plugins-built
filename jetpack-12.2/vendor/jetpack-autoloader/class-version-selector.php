d2ea8664af9048aea99ef5bee5325aa5e73f1ce2
rebeccahum <no-reply@automattic.com>
2023-06-15 02:08:51 +0000

    Built from vip-go-mu-plugins@2709a85d56c16231dc5f07c2dc1addcd2a30effc

--- jetpack-12.2/vendor/jetpack-autoloader/class-version-selector.php
+++ jetpack-12.2/vendor/jetpack-autoloader/class-version-selector.php
@@ -5,7 +5,7 @@
  * @package automattic/jetpack-autoloader
  */
 
-namespace Automattic\Jetpack\Autoloader\jpf11009ded9fc4592b6a05b61ce272b3c_jetpackⓥ12_2;
+namespace Automattic\Jetpack\Autoloader\jpf11009ded9fc4592b6a05b61ce272b3c_jetpackⓥ12_2_1;
 
  // phpcs:ignore
 
