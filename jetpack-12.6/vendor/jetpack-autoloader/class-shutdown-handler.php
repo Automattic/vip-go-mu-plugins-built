fe7a94ec5487ed4b84fb93ad47b7f5d778adc936
sjinks <no-reply@automattic.com>
2023-09-21 20:02:01 +0000

    Built from vip-go-mu-plugins@e3bbd072c638a024cbc0d38e2c66bd135e912576

--- jetpack-12.6/vendor/jetpack-autoloader/class-shutdown-handler.php
+++ jetpack-12.6/vendor/jetpack-autoloader/class-shutdown-handler.php
@@ -5,7 +5,7 @@
  * @package automattic/jetpack-autoloader
  */
 
-namespace Automattic\Jetpack\Autoloader\jpf11009ded9fc4592b6a05b61ce272b3c_jetpackⓥ12_6;
+namespace Automattic\Jetpack\Autoloader\jpf11009ded9fc4592b6a05b61ce272b3c_jetpackⓥ12_6_1;
 
  // phpcs:ignore
 
