63567f6d99b20fdb266ba0f5797289738db356e3
sjinks <no-reply@automattic.com>
2023-11-07 12:33:34 +0000

    Built from vip-go-mu-plugins@2fdbf329f5e61ad70d21104084d7bc8d5f72c26b

--- jetpack-12.8/vendor/jetpack-autoloader/class-latest-autoloader-guard.php
+++ jetpack-12.8/vendor/jetpack-autoloader/class-latest-autoloader-guard.php
@@ -5,7 +5,7 @@
  * @package automattic/jetpack-autoloader
  */
 
-namespace Automattic\Jetpack\Autoloader\jpf11009ded9fc4592b6a05b61ce272b3c_jetpackⓥ12_8_beta\al2_12_0;
+namespace Automattic\Jetpack\Autoloader\jpf11009ded9fc4592b6a05b61ce272b3c_jetpackⓥ12_8\al2_12_0;
 
  // phpcs:ignore
 
