2ceaaf56fc88ccb4f6fc7a08915fcf93284d914a
rebeccahum <no-reply@automattic.com>
2024-12-03 18:20:04 +0000

    Built from vip-go-mu-plugins@7fbda416993d62b952bf823d07f902cc727fcab3

--- search/elasticpress/includes/classes/Elasticsearch.php
+++ search/elasticpress/includes/classes/Elasticsearch.php
@@ -1618,12 +1618,25 @@ class Elasticsearch {
 	 * Query logging. Don't log anything to the queries property when
 	 * WP_DEBUG is not enabled. Calls action 'ep_add_query_log' if you
 	 * want to access the query outside of the ElasticPress plugin. This
-	 * runs regardless of debufg settings.
+	 * runs regardless of debug settings.
 	 *
 	 * @param array $query Query to log.
 	 */
 	protected function add_query_log( $query ) {
-		if ( ( defined( 'WP_DEBUG' ) && WP_DEBUG ) || ( defined( 'WP_EP_DEBUG' ) && WP_EP_DEBUG ) ) {
+		$wp_debug    = defined( 'WP_DEBUG' ) && WP_DEBUG;
+		$wp_ep_debug = defined( 'WP_EP_DEBUG' ) && WP_EP_DEBUG;
+
+		/**
+		 * Filter query logging. Don't log anything to the queries property when true.
+		 *
+		 * @hook ep_disable_query_logging
+		 * @param  {bool} Whether to log to the queries property. Defaults to false.
+		 * @return {bool} New value
+		 * @since  5.2.0
+		 */
+		$disable_query_logging = apply_filters( 'ep_disable_query_logging', false );
+
+		if ( ! $disable_query_logging && ( $wp_debug || $wp_ep_debug ) ) {
 			// phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_wp_debug_backtrace_summary
 			$query['backtrace'] = wp_debug_backtrace_summary( null, 1, false ); // VIP: Search Dev Tools relies on this backtrace
 			$this->queries[]    = $query;
--- search/elasticpress/includes/classes/Feature/Facets/Facets.php
+++ search/elasticpress/includes/classes/Feature/Facets/Facets.php
@@ -220,7 +220,7 @@ class Facets extends Feature {
 			return true;
 		}
 
-		if ( is_admin() ) {
+		if ( is_admin() || is_feed() ) {
 			return false;
 		}
 
